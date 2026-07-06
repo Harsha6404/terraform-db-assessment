ad9eba99e2c6270d598fb4ae02a87c841782a987
Harsha6404 <151925032+Harsha6404@users.noreply.github.com>
2026-07-06 12:25:11 +0530

    Update variables.tf

--- infra/modules/network/variables.tf
+++ infra/modules/network/variables.tf
@@ -2,14 +2,26 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr" {
+variable "public_subnet_1_cidr" {
   default = "10.0.1.0/24"
 }
 
-variable "private_subnet_cidr" {
+variable "public_subnet_2_cidr" {
   default = "10.0.2.0/24"
 }
 
-variable "availability_zone" {
+variable "private_subnet_1_cidr" {
+  default = "10.0.3.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  default = "10.0.4.0/24"
+}
+
+variable "az1" {
   default = "us-east-1a"
+}
+
+variable "az2" {
+  default = "us-east-1b"
 }
