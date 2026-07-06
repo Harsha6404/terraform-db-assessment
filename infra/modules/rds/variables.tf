647c04161bbececdfafa6bef7c44f3e9bb567676
Harsha6404 <151925032+Harsha6404@users.noreply.github.com>
2026-07-06 10:38:40 +0530

    Create variables.tf

---
+++ infra/modules/rds/variables.tf
@@ -0,0 +1,15 @@
+variable "db_name" {
+  default = "hoteldb"
+}
+
+variable "username" {
+  default = "postgres"
+}
+
+variable "password" {
+  default = "Password123!"
+}
+
+variable "subnet_id" {}
+
+variable "security_group_id" {}
