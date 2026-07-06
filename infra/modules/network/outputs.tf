8238d3948aaf16e0705953866de018193674b585
Harsha6404 <151925032+Harsha6404@users.noreply.github.com>
2026-07-06 12:29:19 +0530

    Update outputs.tf

--- infra/modules/network/outputs.tf
+++ infra/modules/network/outputs.tf
@@ -2,10 +2,16 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnet" {
-  value = aws_subnet.public.id
+output "public_subnets" {
+  value = [
+    aws_subnet.public1.id,
+    aws_subnet.public2.id
+  ]
 }
 
-output "private_subnet" {
-  value = aws_subnet.private.id
+output "private_subnets" {
+  value = [
+    aws_subnet.private1.id,
+    aws_subnet.private2.id
+  ]
 }
